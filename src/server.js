1591d812880caa576ac24485a897caa270e20da3
Jairo Junior <jjotta158@outlook.com>
2020-04-17 23:54:03 -0300

    melhorias no servidor

--- src/server.js
+++ src/server.js
@@ -1,10 +1,25 @@
 const port = 3003;
 const express = require("express");
-
+const database = require("./database");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
 app.get('/products', (req, res, next) => {
-    res.send({name:"Noteboook", price:123.45}) //convert to JSON
+    res.json(database.getProducts()) //convert to JSON
+})
+
+app.get('/product/:id',(req, res) => {
+    let product_id = req.params.id_product;
+    res.json(database.getProductById(product_id))
+})
+
+app.post('/addProduct', (req,res) => {
+    res.send(database.saveProduct(req.body))
 })
 
 app.listen(port, () => {
